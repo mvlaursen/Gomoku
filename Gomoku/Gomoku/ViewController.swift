f73205385d16d90aa199667cad190f0194a4c6e3
Mike Laursen <mailbox@laursen.org>
2018-10-28 16:06:17 -0700

    Generated app

---
+++ Gomoku/Gomoku/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gomoku
+//
+//  Created by Mike Laursen on 10/28/18.
+//  Copyright © 2018 Appamajigger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
