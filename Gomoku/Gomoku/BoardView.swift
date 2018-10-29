3f64d1fdda99f9d1bae1fa1646112dc74f3ceb81
Mike Laursen <mailbox@laursen.org>
2018-10-28 19:18:07 -0700

    Shitty board view
    Temporarily putting in a shitty text-based board view. Will be redone with graphics and SpriteKit, but I want to concentrate on game mechanics and logic first.

---
+++ Gomoku/Gomoku/BoardView.swift
@@ -0,0 +1,43 @@
+//
+//  BoardView.swift
+//  Gomoku
+//
+//  Created by Mike Laursen on 10/28/18.
+//  Copyright © 2018 Appamajigger. All rights reserved.
+//
+
+import UIKit
+
+// This text-based view is temporary and will be replaced with a graphic,
+// SpriteKit-based view.
+class BoardView: UITextView {
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.text = """
+            . . . . . . . . . . . . . . .\n
+            . . . . . . . . . . . . . . .\n
+            . . . . . . . . . . . . . . .\n
+            . . . . . . . . . . . . . . .\n
+            . . . . . . . . . . . . . . .\n
+            . . . . . . . . . . . . . . .\n
+            . . . . . . . . . . . . . . .\n
+            . . . . . . . . . . . . . . .\n
+            . . . . . . . . . . . . . . .\n
+            . . . . . . . . . . . . . . .\n
+            . . . . . . . . . . . . . . .\n
+            . . . . . . . . . . . . . . .\n
+            . . . . . . . . . . . . . . .\n
+            . . . . . . . . . . . . . . .\n
+            . . . . . . . . . . . . . . .\n
+            """
+    }
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
