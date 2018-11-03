f88cc23412d31309294db113a4f3ca64720c7c1b
Mike Laursen <mailbox@laursen.org>
2018-11-03 13:46:25 -0700

    Work in progress on drawing playing pieces

--- Gomoku/Gomoku/BoardView.swift
+++ Gomoku/Gomoku/BoardView.swift
@@ -24,11 +24,11 @@ class BoardView: UIView {
     }
     
     func drawGrid(rect: CGRect, color: UIColor, firstRank: Int, numRanks: Int) {
-        color.set()
-        
         let squareDim = rect.size.width / CGFloat(Board.paddedBoardDim + 1)
         let lineStart = CGFloat(firstRank + 1) * squareDim
         let lineLength = CGFloat(firstRank + numRanks) * squareDim
+
+        color.set()
 
         for rank in firstRank..<firstRank + numRanks {
             let lineOffset = CGFloat(rank + 1) * squareDim
@@ -46,6 +46,17 @@ class BoardView: UIView {
             vertLine.stroke()
         }
     }
+    
+    func drawStone(rect: CGRect, color: UIColor, row: Int, col: Int) {
+        let squareDim = rect.size.width / CGFloat(Board.paddedBoardDim + 1)
+        let stone = UIBezierPath()
+
+        color.set()
+        stone.addArc(withCenter: CGPoint(x: CGFloat(row) * squareDim, y: CGFloat(col) * squareDim), radius: squareDim / 3.0, startAngle: 0, endAngle: CGFloat(2 * M_PI), clockwise: true)
+        stone.close()
+        stone.fill()
+        stone.stroke()
+    }
 
     // Again, this is temporary silliness, just so we can see the board being
     // redrawn with each game turn.
@@ -57,5 +68,12 @@ class BoardView: UIView {
 
         drawGrid(rect: rect, color: UIColor.lightGray, firstRank: 0, numRanks: Board.paddedBoardDim)
         drawGrid(rect: rect, color: BoardView.colors.randomElement()!, firstRank: 4, numRanks: Board.playableBoardDim)
+        
+        drawStone(rect: rect, color: UIColor.black, row: 5, col: 5)
+        drawStone(rect: rect, color: UIColor.black, row: 6, col: 6)
+        drawStone(rect: rect, color: UIColor.black, row: 7, col: 7)
+        drawStone(rect: rect, color: UIColor.black, row: 8, col: 8)
+        drawStone(rect: rect, color: UIColor.black, row: 9, col: 9)
+
     }
 }
