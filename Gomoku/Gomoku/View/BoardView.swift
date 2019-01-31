3c0b41771a795833bf634228a1fc49dae48fd53f
Mike Laursen <mailbox@laursen.org>
2019-01-31 15:47:59 -0800

    Make stones wider.
    I did some research and it turns out Go stones are about the same width as the spacing between the board's grid lines, so that the stones touch each other. When I tried making them the same width as "squareDim", it didn't look good to me, so I made the stones slightly smaller so one can see a small gap between them.

--- Gomoku/Gomoku/View/BoardView.swift
+++ Gomoku/Gomoku/View/BoardView.swift
@@ -21,17 +21,17 @@ class BoardView: SKView {
         let width = min(size.width, size.height)
         
         if width.isEqual(to: 375.0) {
-            return BoardMetrics(boardImageName: "GomokuBoard368", squareDim: 23.0, stoneDim: 11.5)
+            return BoardMetrics(boardImageName: "GomokuBoard368", squareDim: 23.0, stoneDim: 21.0)
         } else if width.isEqual(to: 768.0) {
-            return BoardMetrics(boardImageName: "GomokuBoard768", squareDim: 48.0, stoneDim: 24.0)
+            return BoardMetrics(boardImageName: "GomokuBoard768", squareDim: 48.0, stoneDim: 45.0)
         } else if width.isEqual(to: 414.0) {
-            return BoardMetrics(boardImageName: "GomokuBoard400", squareDim: 25.0, stoneDim: 12.5)
+            return BoardMetrics(boardImageName: "GomokuBoard400", squareDim: 25.0, stoneDim: 23.0)
         } else if width.isEqual(to: 1024.0) {
-            return BoardMetrics(boardImageName: "GomokuBoard1024", squareDim: 64.0, stoneDim: 32.0)
+            return BoardMetrics(boardImageName: "GomokuBoard1024", squareDim: 64.0, stoneDim: 60.0)
         } else if width.isEqual(to: 834.0) {
-            return BoardMetrics(boardImageName: "GomokuBoard768", squareDim: 48.0, stoneDim: 24.0)
+            return BoardMetrics(boardImageName: "GomokuBoard768", squareDim: 48.0, stoneDim: 45.0)
         } else {
-            return BoardMetrics(boardImageName: "GomokuBoard320", squareDim: 20.0, stoneDim: 10.0)
+            return BoardMetrics(boardImageName: "GomokuBoard320", squareDim: 20.0, stoneDim: 18.0)
         }
     }
     
@@ -41,9 +41,30 @@ class BoardView: SKView {
             board.anchorPoint = CGPoint(x: 0.5, y: 0.5)
         
             let metrics = BoardView.boardMetrics()
+            
             let image = SKSpriteNode(imageNamed: metrics.boardImageName)
             board.addChild(image)
+            
+            let black = SKSpriteNode(imageNamed: "1024px-Realistic_Go_Stone.svg")
+            black.position = CGPoint(x: metrics.squareDim, y: 0.0)
+            black.size = CGSize(width: metrics.stoneDim, height: metrics.stoneDim)
+            board.addChild(black)
         
+            let white = SKSpriteNode(imageNamed: "1024px-Realistic_White_Go_Stone.svg")
+            white.position = CGPoint(x: metrics.squareDim, y: metrics.squareDim)
+            white.size = CGSize(width: metrics.stoneDim, height: metrics.stoneDim)
+            board.addChild(white)
+            
+            let black2 = SKSpriteNode(imageNamed: "1024px-Realistic_Go_Stone.svg")
+            black2.position = CGPoint(x: 2.0 * metrics.squareDim, y: 0.0)
+            black2.size = CGSize(width: metrics.stoneDim, height: metrics.stoneDim)
+            board.addChild(black2)
+
+            let white2 = SKSpriteNode(imageNamed: "1024px-Realistic_White_Go_Stone.svg")
+            white2.position = CGPoint(x: 2.0 * metrics.squareDim, y: metrics.squareDim)
+            white2.size = CGSize(width: metrics.stoneDim, height: metrics.stoneDim)
+            board.addChild(white2)
+            
             self.presentScene(board)
         }
     }
