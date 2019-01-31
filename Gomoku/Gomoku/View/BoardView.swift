5c34cb2cc6d9a5db16d5ec43914896c2db96de57
Mike Laursen <mailbox@laursen.org>
2019-01-31 12:34:04 -0800

    BoardView uses device size to decide which graphical assets to use.

--- Gomoku/Gomoku/View/BoardView.swift
+++ Gomoku/Gomoku/View/BoardView.swift
@@ -10,12 +10,38 @@ import SpriteKit
 import UIKit
 
 class BoardView: SKView {
+    struct BoardMetrics {
+        let boardImageName: String
+        let squareDim: CGFloat
+        let stoneDim: CGFloat
+    }
+    
+    static func boardMetrics() -> BoardMetrics {
+        let size = UIScreen.main.bounds.size
+        let width = min(size.width, size.height)
+        
+        if width.isEqual(to: 375.0) {
+            return BoardMetrics(boardImageName: "GomokuBoard368", squareDim: 23.0, stoneDim: 11.5)
+        } else if width.isEqual(to: 768.0) {
+            return BoardMetrics(boardImageName: "GomokuBoard768", squareDim: 48.0, stoneDim: 24.0)
+        } else if width.isEqual(to: 414.0) {
+            return BoardMetrics(boardImageName: "GomokuBoard400", squareDim: 25.0, stoneDim: 12.5)
+        } else if width.isEqual(to: 1024.0) {
+            return BoardMetrics(boardImageName: "GomokuBoard1024", squareDim: 64.0, stoneDim: 32.0)
+        } else if width.isEqual(to: 834.0) {
+            return BoardMetrics(boardImageName: "GomokuBoard768", squareDim: 48.0, stoneDim: 24.0)
+        } else {
+            return BoardMetrics(boardImageName: "GomokuBoard320", squareDim: 20.0, stoneDim: 10.0)
+        }
+    }
+    
     override func layoutSubviews() {
         if self.scene == nil {
             let board = SKScene(size: self.bounds.size)
             board.anchorPoint = CGPoint(x: 0.5, y: 0.5)
         
-            let image = SKSpriteNode(imageNamed: "board.png")
+            let metrics = BoardView.boardMetrics()
+            let image = SKSpriteNode(imageNamed: metrics.boardImageName)
             board.addChild(image)
         
             self.presentScene(board)
