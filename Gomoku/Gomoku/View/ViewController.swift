a97d64ffaa85cebde697efd856c05eaf6032bef8
Mike Laursen <mailbox@laursen.org>
2019-02-13 20:04:20 -0800

    Finishing touches on Creative Mode.
    Not that Creative Mode couldn't use refinements like more than one level of undo, multiple colors, etc. It's just that Creative Mode was a distraction to entertain the kids, so I want to get back to other modes.

--- Gomoku/Gomoku/View/ViewController.swift
+++ Gomoku/Gomoku/View/ViewController.swift
@@ -12,24 +12,35 @@ class ViewController: UIViewController {
     @IBOutlet weak var buttons: UIStackView!
     @IBOutlet var playButton: UIButton!
     @IBOutlet var quitButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet var undoButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.quitButton.removeFromSuperview()
-        self.undoButton.removeFromSuperview()
+        
+        // Remove the Play button, which is only there for computer vs.
+        // computer mode, which isn't even implemented yet. Well, it is, only
+        // using the old, non-SpriteKit UI.
+        self.playButton.removeFromSuperview()
+        
+        titleLabel.text = "Gomoku (Creative Mode)"
+        
+        self.play()
     }
 
-    @IBAction func play(_ sender: UIButton) {
-        setButtons(showPlayButton: false)
-        
+    private func play() {
         let boardViews = self.view.subviews.filter { $0 is BoardView }
         precondition(boardViews.count == 1)
         let boardView = boardViews[0] as! CreativeBoardView
         boardView.play {
-            self.setButtons(showPlayButton: true)
+//            self.setButtons(showPlayButton: true)
         }
+    }
+
+    @IBAction func play(_ sender: UIButton) {
+        setButtons(showPlayButton: false)
+        self.play()
     }
     
     @IBAction func quit(_ sender: Any) {
@@ -38,7 +49,7 @@ class ViewController: UIViewController {
         let boardView = boardViews[0] as! CreativeBoardView
         boardView.quit()
 
-        setButtons(showPlayButton: true)
+//        setButtons(showPlayButton: true)
     }
     
     @IBAction func undo(_ sender: Any) {
